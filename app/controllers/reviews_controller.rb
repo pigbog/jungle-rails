3541752bb9042431d2e559c7cef330bb464cafa7
pigbog <mesteves@gmail.com>
2019-02-23 22:32:34 +0000

    Now accepted entered values

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @review= @product.reviews.new({product_id: 23, user_id: 1, description: 'hi there', rating: 1})
+    @review= @product.reviews.new({product_id: params[:product_id], user_id: session[:user_id], description: params[:review]["description"], rating: 1})
       # params[:review]["rating"])
     if @review.save
       redirect_to [:products]
     else
-      redirect_to [:product]
+      redirect_to [:products]
     end
 
 end
