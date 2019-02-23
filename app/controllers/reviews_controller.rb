7215eae402ce095665f27869b48f8dee1c616669
pigbog <mesteves@gmail.com>
2019-02-23 22:25:33 +0000

    User reviews now accepted hard coded values

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,22 @@
 class ReviewsController < ApplicationController
   def create
-    
-  end
+    @product = Product.find(params[:product_id])
+    @review= @product.reviews.new({product_id: 23, user_id: 1, description: 'hi there', rating: 1})
+      # params[:review]["rating"])
+    if @review.save
+      redirect_to [:products]
+    else
+      redirect_to [:product]
+    end
+
+end
+
+#     @category= Category.new(category_params)
+
+# if @category.save
+#   redirect_to [:admin, :categories], notice: 'Category created!'
+# else
+#   render :new
+# end
+
 end
